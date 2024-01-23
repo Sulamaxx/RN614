b579b4ebabca56221720cd77602ee8f665f5a109
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2024-01-23 14:13:33 +0530

    a

--- order_process.php
+++ order_process.php
@@ -31,7 +31,28 @@ if (!empty($errors)) {
     header("Location: fix_order.php?errors=" . $errorString);
     exit();
 } else {
-    // should add data to database in here
+
+    $conn->query("CREATE DATABASE IF NOT EXISTS `shop_db`");
+    $conn->query("USE `shop_db`");
+    $conn->query("CREATE TABLE IF NOT EXISTS `orders` (
+        `order_id` int NOT NULL AUTO_INCREMENT,
+        `fname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+        `lname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+        `email` varchar(255) DEFAULT NULL,
+        `phone` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+        `address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+        `pcode` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+        `city` varchar(50) DEFAULT NULL,
+        `state` varchar(50) DEFAULT NULL,
+        `qty` int DEFAULT NULL,
+        `product` varchar(255) DEFAULT NULL,
+        `unit_price` double DEFAULT NULL,
+        `order_cost` double DEFAULT NULL,
+        `order_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+        `order_status` varchar(50) DEFAULT 'PENDING',
+        PRIMARY KEY (`order_id`)
+      ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3");
+
     $query = "INSERT INTO `orders`(`fname`,`lname`,`email`,`phone`,`address`,`pcode`,`city`,`state`,`qty`,`product`,`unit_price`,`order_cost`) 
 VALUES('" . $firstName . "','" . $lastName . "','" . $email . "','" . $mobile . "','" . $address . "','" . $postalCode . "','" . $city . "','" . $state . "','" . $quantity . "','" . $product_name . "','" . $product_price . "','" . $total_price . "');
 ";
